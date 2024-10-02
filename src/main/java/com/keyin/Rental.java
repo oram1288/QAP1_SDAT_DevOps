5fbf9043dce7b7cfca1c11b9d406b7d9907b9662
oram1288 <cody.oram@keyin.com>
2024-10-02 12:42:10 -0230

    Rental Class

--- src/main/java/com/keyin/Rental.java
+++ src/main/java/com/keyin/Rental.java
@@ -1,4 +1,30 @@
 package com.keyin;
 
 public class Rental {
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+        movie.rentMovie();
+    }
+
+    // Getters
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+
+    // Setters
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public void setDaysRented(int daysRented) {
+        this.daysRented = daysRented;
+    }
 }
---
+++ src/main/java/com/keyin/RentalSystem.java
@@ -0,0 +1,16 @@
+package com.keyin;
+
+public class RentalSystem {
+    public static void main(String[] args) {
+        Customer customer = new Customer("Cody Oram", "cody@gmail.com", 709123123);
+        Movie movie1 = new Movie("Avengers: Endgame", "Sci-Fi", "3h 1m", "Anthony Russo, Joe Russo");
+        Movie movie2 = new Movie("The Dark Knight","Action","2h 32m", "Christopher Nolan");
+
+        customer.rentMovie(movie1,5);
+        customer.rentMovie(movie2,3);
+
+        for (Rental rental : customer.getRentals()) {
+            System.out.println("Movie: " + rental.getMovie().getTitle() + ", Days Rented: " + rental.getDaysRented());
+        }
+    }
+}
